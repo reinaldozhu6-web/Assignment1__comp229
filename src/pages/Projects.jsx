86648d51f9f5c21daaa64d75441fb934017ab945
reinaldozhu6-web <reinaldozhu6@gmail.com>
2025-09-12 20:08:54 -0400

    testing version

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+const Projects = () => {
+    return (
+        <div>
+            <h1>My Projects</h1>
+            <p>These are my projects.</p>
+        </div>
+    );
+};
+
+export default Projects;
---
+++ src/pages/Services.jsx
@@ -0,0 +1,10 @@
+const Services = () => {
+    return (
+        <div>
+            <h1>Services</h1>
+            <p>These are the services I offer.</p>
+        </div>
+    );
+};
+
+export default Services;
